ee708aa15fc72463921adb28e94646185b61e2dd
NehaSawant26 <163569218+NehaSawant26@users.noreply.github.com>
2024-05-24 12:53:29 +0530

    Update TextForm.jsx

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import { useState } from 'react'
 
-
-
-
 export default function TextForm(props) {
     const [text, setText] = useState('Enter text here');
 
-
     const handleUpClick = () => {
-
         let newText = text.toUpperCase();
         setText(newText);
         props.showAlert("Converted to Upper Case", "success")
@@ -17,21 +12,18 @@ export default function TextForm(props) {
     }
 
     const handleLoClick = () => {
-
         let newText = text.toLowerCase();
         setText(newText);
         props.showAlert("Converted to Lower Case", "success")
     }
 
     const handleClearClick = () => {
-
         let newText = '';
         setText(newText);
         props.showAlert("Text has been cleared", "success")
     }
 
     const handleOnChange = (event) => {
-
         setText(event.target.value);
     }
     
@@ -53,9 +45,7 @@ export default function TextForm(props) {
         <>
             <div className="container" style={{color: props.mode ==='dark'? 'white': '#042743'}}>
                 <h3>{props.heading}</h3>
-
                 <div className="mb-3">
-
                     <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode ==='dark'? '#042743': 'white',color: props.mode ==='dark'? 'white': 'black'}} id="myBox" rows="8" cols="30"></textarea>
                 </div>
                 <button disabled={text.length === 0} className="btn btn-primary mx-2 my-2" onClick={handleUpClick}>Convert to UpperCase</button>
